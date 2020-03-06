163b14f11feeb19fa633bd63631e7bc4b33f3f25
Iranzu <iranzuaguirre@hotmail.com>
2020-03-06 11:11:03 +0100

    Añadir campos

--- resources/views/animales.blade.php
+++ resources/views/animales.blade.php
@@ -15,13 +15,54 @@
 
 				<form action="{{url('/animal')}}" method="POST" class="form-horizontal">
 					{{csrf_field()}}
-					<div class="form-group">
+					<div class="form-group col-md-12 control-label">
 						<label for="numchip">NumChip: </label>
 						<input type="text" class="form-control" name="numchip" value="{{old('numchip')}}">
 					</div>
-					<div class="form-group">
+					<div class="form-group col-md-12 control-label">
 						<label for="nombre">Nombre: </label>
 						<input type="text" class="form-control" name="nombre" value="{{old('nombre')}}">
+					</div>
+					<div class="form-group col-md-12 control-label">
+						<label for="edad">Edad: </label>
+						<input type="number" class="form-control" name="edad" value="{{old('edad')}}">
+					</div>
+					<div class="form-group col-md-12 control-label">
+						<label for="genero">Género: </label>
+						<div class="col-md-12" id="genero" name="genero">
+							<div class="radio">
+								<label for="genero-0">
+									<input type="radio" name="genero" id="genero-0" value="Macho">Macho
+								</label>
+							</div>
+							<div class="radio">
+								<label for="genero-1">
+									<input type="radio" name="genero" id="genero-1" value="Hembra">Hembra
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="form-group col-md-12 control-label">
+						<label for="raza">Raza: </label>
+						<input type="text" class="form-control" name="raza" value="{{old('raza')}}">
+					</div>
+					<div class="form-group col-md-12 control-label">
+						<label for="salud">Salud: </label>
+						<div>
+							<select id="salud" name="salud" class="form-control">
+								<?php
+								$valores = ["Buena Salud", "Mala Salud"];
+								foreach ($valores as $clave => $val) {
+									echo "<option value='$clave'>$val</option>";
+								}
+								?>
+
+							</select>
+						</div>
+					</div>
+					<div class="form-group col-md-12 control-label">
+						<label for="descripcion">Descripcion: </label>
+						<textarea class="form-control" id="descripcion" name="descripcion" placeholder="Introduzca una breve descripción"></textarea>
 					</div>
 					<button type="submit" class="btn btn-primary">Añadir</button>
 				</form>
